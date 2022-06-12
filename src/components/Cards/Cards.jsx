89b7225f35da7aced3bac25269f6ae7ceb276252
RAHUL <72123572+rahul-jalandhra@users.noreply.github.com>
2022-06-12 03:26:25 -0700

    try this type props

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,7 +3,7 @@ import { Typography, Grid } from '@material-ui/core';
 import CardComponent from './Card/Card';
 import styles from './Cards.module.css';
 
-const Info = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+const Info = ( { confirmed, recovered, deaths, lastUpdate } ) => {
   if (!confirmed) {
     return 'Loading...';
   }
